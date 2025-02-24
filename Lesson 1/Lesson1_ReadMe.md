f3618c50defe0d358769aca0e3bca502fc7d7054
pma365 <appleamirbahador@gmail.com>
2025-02-24 12:42:12 +0330

    updateLesson1

--- Lesson 1/Lesson1_ReadMe.md
+++ Lesson 1/Lesson1_ReadMe.md
@@ -5,3 +5,46 @@
 ##### so you need to allocate memory first for declaring variables
 
 ---
+
+#### Integer types :
+
+char c; // 1 byte
+short s; // 2 bytes
+int i; // 4 bytes
+long l; // 4 bytes or 8 bytes
+long long ll; // 8 bytes
+
+#### Floating point types :
+
+float f; // 4 bytes
+double d; // 8 bytes
+long double ld; // 10 bytes or 16 bytes
+
+#### Pointer types :
+
+int\* p; // 4 bytes or 8 bytes
+
+#### Array types :
+
+int arr[10]; // 40 bytes (10 x 4 bytes)
+
+#### Structure types :
+
+struct {
+int a; // 4 bytes
+char b; // 1 byte
+short c; // 2 bytes
+}myStruct; // 7 bytes (4 + 1 + 2)
+
+> Output:
+> Size of char: 1 bytes
+> Size of short: 2 bytes
+> Size of int: 4 bytes
+> Size of long: 4 bytes
+> Size of long long: 8 bytes
+> Size of float: 4 bytes
+> Size of double: 8 bytes
+> Size of long double: 16 bytes
+> Size of pointer: 8 bytes
+> Size of array: 40 bytes
+> Size of structure: 8 bytes
--- Lesson 1/code.c
+++ Lesson 1/code.c
@@ -1,3 +1,42 @@
-int main(){
-    
+#include <stdio.h>
+
+int main() {
+    // Integer types
+    char c;         // 1 byte
+    short s;        // 2 bytes
+    int i;          // 4 bytes
+    long l;         // 4 bytes or 8 bytes
+    long long ll;   // 8 bytes
+
+    // Floating point types
+    float f;        // 4 bytes
+    double d;       // 8 bytes
+    long double ld; // 10 bytes or 16 bytes
+
+    // Pointer types
+    int* p;         // 4 bytes or 8 bytes
+
+    // Array types
+    int arr[10];    // 40 bytes (10 x 4 bytes)
+
+    // Structure types
+    struct {
+        int a;      // 4 bytes
+        char b;     // 1 byte
+        short c;    // 2 bytes
+    }myStruct;            // 7 bytes (4 + 1 + 2)
+
+    printf("Size of char: %zu bytes\n", sizeof(char));
+    printf("Size of short: %zu bytes\n", sizeof(short));
+    printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of long: %zu bytes\n", sizeof(long));
+    printf("Size of long long: %zu bytes\n", sizeof(long long));
+    printf("Size of float: %zu bytes\n", sizeof(float));
+    printf("Size of double: %zu bytes\n", sizeof(double));
+    printf("Size of long double: %zu bytes\n", sizeof(long double));
+    printf("Size of pointer: %zu bytes\n", sizeof(int*));
+    printf("Size of array: %zu bytes\n", sizeof(arr));
+    printf("Size of structure: %zu bytes\n", sizeof(myStruct));
+
+    return 0;
 }
